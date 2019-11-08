69dc67b10fc930d2abc2bd17561b97804649e9da
yasin <ybalcanci@gmail.com>
2019-11-08 18:17:19 +0300

    Request target fix, indentation correction, method addition.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component<{}, { messages: Array<any>, isLoading: boolean }>{
-  constructor(props: any){
-    super(props);
-    this.state = {
-      messages: [],
-      isLoading: false
+class App extends React.Component<{}, { messages: Array<any>, isLoading: boolean, text: string }> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            messages: [],
+            isLoading: false,
+            text: ""
+        }
     }
-  }
-  componentDidMount(): void {
-    this.setState({ isLoading: true });
-    fetch('http://localhost:8080/messages')
-        .then(response => response.json())
-        .then(data => this.setState({ messages: data, isLoading: false }));
-  }
 
-  render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-    const {messages, isLoading} = this.state;
-    if(isLoading){
-      return <p>Loading...</p>;
+    componentDidMount(): void {
+        this.getMessages()
     }
-    return (
-        <div className="App">
-          {messages.map((message: any) =>
-              <div key={message.id}>
-                {message.text}
-              </div>
-          )}
-        </div>
-    );
-  }
+
+    getMessages = async () => {
+        this.setState({isLoading: true});
+        fetch('http://localhost:8080', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => this.setState({messages: data, isLoading: false}));
+    }
+
+    sendMessage = async () => {
+        await fetch('http://localhost:8080', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({text: "sa", sender: {id: 1, username: 'ybal'}})
+        });
+        this.clearTextArea()
+    }
+
+    clearTextArea = () => {
+        this.setState({
+            text: ""
+        })
+    }
+
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        const {messages, isLoading} = this.state;
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+        return (
+            <div className="App">
+                {messages.map((message: any) =>
+                    <div key={message.id}>
+                        {message.text}
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
